7194cf6e0a91ba289cf540aa4b92d8d6c7149953
liZn <1063383308@qq.com>
2018-06-12 11:27:30 +0800

    hah

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const ejs = require("ejs");
+const consolidate = require("consolidate");
+const bodyParse = require("body-parser");
+const db = require("./models/db");
+const server = express();
+
+//模板
+server.set("views","./views");//模版文件夹
+server.set("view engine","html");//渲染成什么格式
+server.engine("html",consolidate.ejs);//使用什么模版引擎
+
+//静态资源文件
+server.use(express.static("./public"));
+server.use(bodyParse.urlencoded({extended:false}))
+server.get("/",(req,res)=>{
+	res.render("index.ejs",{})
+})
+server.post("/tijiao",(req,res)=>{
+	db.insertOne("liu","yan",{"name":req.body.name,"text":req.body.yiyan},(err,result)=>{
+		if(err){
+			res.send({"status":0,data:"失败"}).end()
+		}else{
+			res.send({"status":1,request:result,data:"成功"}).end();
+		}
+	})
+})
+server.post("/du",(req,res)=>{
+	db.find("liu","yan",{},{"displayNum":0,"pagesNum":0},(err,result)=>{
+		if(err){
+			res.send({"status":0,request:null,data:"失败"})
+		}else{
+			res.send({"status":1,request:result,data:"成功"})
+		}
+		
+	})
+});
+// server.get("/",(req,res)=>{
+// 	db.insertOne("liu","yan",{"name":"小鑫鑫","text":"hello world","id":2},(err,result)=>{
+// 		if(err){
+// 			res.send("失败失败").end()
+// 		}else{
+// 			res.send('成功').end()
+// 		}
+// 	})
+// });
+server.get("/delete",(req,res)=>{
+	db.deleteData("liu","yan",{"id":1},(err,result)=>{
+		if(err){
+			res.send('cuola').end()
+		}else{
+			res.send("ok").end()
+		}
+	})
+});
+server.get("/change",(req,res)=>{
+	db.changeData("liu","yan",{"name":"lizn"},{$set:{"name":"liznn.cn"}},(err,result)=>{
+		if(err){
+			res.send(err).end()
+		}else{
+			res.send(result).end()
+		}
+	})
+});
+server.get("/look",(req,res)=>{
+	db.find("liu","yan",{"name":"小鑫鑫"},{"displayNum":2,"pagesNum":0},(err,result)=>{
+		if(err){
+			res.send(err).end()
+		}else{
+			res.send(result).end()
+		}
+	})
+});
+
+server.listen(8088)
---
+++ models/db.js
@@ -0,0 +1,116 @@
+const mongoClient = require("mongodb").MongoClient;
+const url = require("./seting");
+//连接数据库
+function _connectDB(callback){
+	mongoClient.connect(url.url,(err,db)=>{
+		if(err){
+			callback(err,null);
+			db.close();
+		}
+		callback(null,db)
+	})
+};
+//增
+exports.insertOne = function(ku,collectionName,json,callback){
+	_connectDB((err,db)=>{
+		if(err) throw err;
+		var dbo = db.db(ku);
+		dbo.collection(collectionName).insertOne(json,(err2,res)=>{
+			if(err2){
+				callback(err2,null);
+				db.close();
+			}else{
+				callback(null,res);
+				db.close();//关闭数据库
+			}
+		})
+	})
+};
+//删
+exports.deleteData = function(ku,collectionName,json,callback){
+	_connectDB((err,db)=>{
+		if(err) throw err;
+		var dbo = db.db(ku);
+		dbo.collection(collectionName).deleteMany(json,(err2,res)=>{
+			if(err2){
+				callback(err2,null);
+				db.close();
+			}else{
+				callback(null,res);
+				db.close();
+			}
+		})
+	})
+};
+//改
+exports.changeData = function(ku,collectionName,json1,json2,callback){
+	_connectDB((err,db)=>{
+		if(err) throw err;
+		var dbo = db.db(ku);
+		dbo.collection(collectionName).updateMany(json1,json2,(err2,res)=>{
+			if(err2){
+				callback(err2,null);
+				db.close();
+			}else{
+				callback(null,res);
+				db.close();
+			}
+		})
+	})
+};
+//查
+exports.find = function(ku,collectionName,json,args,callback){
+	//args定义了页面显示的数量
+	_connectDB((err,db)=>{
+		if(err){
+			callback(err,null);
+			db.close();
+			return;
+		}
+		var dbo = db.db(ku);
+		if(arguments.length!=5){
+			callback("请输入5个参数，少一个都不行！宝贝",null);
+			db.close();
+		}else{
+			var result = []
+			var displayNum = parseInt(args.displayNum);//页面显示数量
+			var pagesNum = parseInt(args.pagesNum);//页面中的页数
+			var allDatas = dbo.collection(collectionName).find(json).limit(displayNum).skip(pagesNum);
+			allDatas.each((err,doc)=>{
+				if(doc!=null){
+					result.push(doc)
+				}else{
+					callback(null,result);
+					db.close();
+				}
+			})
+		}
+		
+	})
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
